01241d9171b511bf9be06b958b44e94472914a73
ssolusa <silas.sol@colorado.edu>
2025-05-18 18:56:54 -0600

    Cleaned up affected files.

--- examples/autotest-validate/autotest-validate.c
+++ examples/autotest-validate/autotest-validate.c
@@ -21,12 +21,13 @@ bool this_function_returns_false()
     return false;
 }
 
-/**
+/*
  * @return a string which contains the username you use for
  * git submissions.  This string should match the string in conf/username.txt
- */
+*/
+ 
 const char *my_username()
 {
-    return "ssolusa";
     // return "todo-please-enter-your-username-here-in-my_username";
+    return "ssolusa";
 }
